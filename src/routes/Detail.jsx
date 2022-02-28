c934d85081901ca44e29f925c66f13d139381124
Eavan <eavan.hsk@gmail.com>
2022-02-27 17:03:18 -0800

    chage background color

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 const Container = styled.div`
   height: 100vh;
-  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  background-image: linear-gradient(-45deg, #6154d7, #000000);
   width: 100%;
   display: flex;
   justify-content: space-around;
